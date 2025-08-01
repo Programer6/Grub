764ef243b58aaadc07ba4239f2b14ecca65de0e0
Programer6 <delroyprithvi@gmail.com>
2025-08-01 20:43:59 +0200

    .

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,14 @@
+
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  root: 'src', 
+  
+  plugins: [tailwindcss()],
+
+  build: {
+    outDir: '../dist', 
+    emptyOutDir: true,
+  }
 })
